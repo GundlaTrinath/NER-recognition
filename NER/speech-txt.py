b7a67c234099316109521c817efafcc9a0b2c9e0
Trinath Gundla <84904193+GundlaTrinath@users.noreply.github.com>
2021-07-05 20:15:39 +0530

    NER

--- NER/speech-txt.py
+++ NER/speech-txt.py
@@ -33,7 +33,7 @@ def get_large_audio_transcription_hin(path='extracted.wav',r=sr.Recognizer()):
                 text=f"{text.capitalize()}. "
                 print(chunk_filename,":",text)
                 whole_text+=text
-    wait1.config(text='Extracting Completed')
+    wait1.config(text='Extracted Completed')
 
     return whole_text
 
@@ -62,7 +62,7 @@ def get_large_audio_transcription_eng(path='extracted.wav',r=sr.Recognizer()):
                 text=f"{text.capitalize()}. "
                 print(chunk_filename,":",text)
                 whole_text+=text
-    wait2.config(text='Extracting Completed')
+    wait2.config(text='Extracted Completed')
 
     return whole_text
 
@@ -77,15 +77,14 @@ def text1():
 
 root=Tk()
 root.title('Video to text Conversion')
-root.configure(background='white')
 root.iconbitmap(r'video_fiu_icon.ico')
-root.geometry('1580x1000')
+root.geometry('1580x887')
 
 
 # ---------extracting audio from video---------
 def video1():
     global wait1
-    wait1 = Label(root, text='Please wait a moment', bg='white', fg='#001a33', font=('Arial', 15,'bold'))
+    wait1 = Label(root, text='Please wait a moment', bg='black', fg='white', font=('Arial', 13,'bold'))
     wait1.pack()
     video=askopenfilename()
     video=VideoFileClip(video)
@@ -96,7 +95,7 @@ def video1():
 # ---------extracting audio from video---------
 def video2():
     global wait2
-    wait2 = Label(text='Please wait a moment', bg='white', fg='#001a33', font=('Arial', 15,'bold'))
+    wait2 = Label(text='Please wait a moment', bg='black', fg='white', font=('Arial', 13,'bold'))
     wait2.pack()
     video=askopenfilename()
     video=VideoFileClip(video)
@@ -116,18 +115,20 @@ def audio2():
     doc=gTTS(text=whole_text,lang='en',slow=False)
     doc.save('Output_audio.wav')
 
-
+my_bg=ImageTk.PhotoImage(Image.open("bgd.png"))
+my_bglabel=Label(image=my_bg).place(x=-5,y=0)
 my_img=ImageTk.PhotoImage(Image.open("logo.png"))
-my_label=Label(image=my_img,border=0).pack(pady=50)
-my_text1=Label(root,text='Select video (HINDI)',bg="white",fg='#001a33',font=('Arial',10,'bold')).place(x=1110,y=200)
-my_text2=Label(root,text='Select video (ENGLISH)',bg="white",fg='#001a33',font=('Arial',10,'bold')).place(x=310,y=200)
-label = Label(root, text='Upload a Video', bg='white', fg='#001a33', font=('Arial', 10)).pack()
-button1=Button(root,width=20,height=2,bg='#001a33',fg='white',text='Browse',font=('Arial',10,'bold'),command=video1).place(x=1100,y=250)
-button2=Button(root,width=20,height=2,bg='#001a33',fg='white',text='Browse',font=('Arial',10,'bold'),command=video2).place(x=300,y=250)
-button3=Button(root,width=30,height=2,bg='#001a33',fg='white',text='Download Audio file ↓\n(Hindi)',font=('Arial',15,'bold'),command=audio1).place(x=0,y=500)
-button4=Button(root,width=30,height=2,bg='#001a33',fg='white',text='Download Audio file ↓\n(English)',font=('Arial',15,'bold'),command=audio2).place(x=400,y=500)
-button5=Button(root,width=30,height=2,bg='#001a33',fg='white',text='Download Text file ↓\n(Hindi)',font=('Arial',15,'bold'),command=text1).place(x=800,y=500)
-button6=Button(root,width=30,height=2,bg='#001a33',fg='white',text='Download Text file ↓\n(English)',font=('Arial',15,'bold'),command=text2).place(x=1200,y=500)
+my_label=Label(image=my_img,border=0).pack(pady=100)
+my_text1=Label(root,text='Select video (HINDI)',bg="black",fg='white',font=('Arial',13,'bold')).place(x=1100,y=250)
+my_text2=Label(root,text='Select video (ENGLISH)',bg="black",fg='white',font=('Arial',13,'bold')).place(x=285,y=250)
+label = Label(root, text='Upload a Video', bg='black', fg='white', font=('Arial', 13)).pack()
+button1=Button(root,width=20,height=2,bg='#df514a',fg='white',border=0,text='Browse',font=('Arial',10,'bold'),command=video1).place(x=1100,y=300)
+button2=Button(root,width=20,height=2,bg='#df514a',fg='white',border=0,text='Browse',font=('Arial',10,'bold'),command=video2).place(x=300,y=300)
+button3=Button(root,width=30,height=2,bg='#df514a',fg='white',border=0,text='Download Audio file ↓\n(Hindi)',font=('Arial',15,'bold'),command=audio1).place(x=0,y=500)
+button4=Button(root,width=30,height=2,bg='#df514a',fg='white',border=0,text='Download Audio file ↓\n(English)',font=('Arial',15,'bold'),command=audio2).place(x=400,y=500)
+button5=Button(root,width=30,height=2,bg='#df514a',fg='white',border=0,text='Download Text file ↓\n(Hindi)',font=('Arial',15,'bold'),command=text1).place(x=800,y=500)
+button6=Button(root,width=30,height=2,bg='#df514a',fg='white',border=0,text='Download Text file ↓\n(English)',font=('Arial',15,'bold'),command=text2).place(x=1200,y=500)
+my_text3=Label(root,text='STEPS TO FOLLOW :-',bg="grey",fg='black',font=('Arial',15,'bold')).place(x=10,y=630)
 
 
 root.mainloop()
