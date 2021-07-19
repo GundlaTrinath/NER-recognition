95f320f6a593da177721b293ce2996406e72b1cf
kskrao219 <66875322+kskrao219@users.noreply.github.com>
2021-07-19 21:08:15 +0530

    Update

--- NER-keshav/GUI/GUIAPP2.py
+++ NER-keshav/GUI/GUIAPP2.py
@@ -7,8 +7,10 @@ import os
 from PIL import ImageTk,Image
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-
-
+import nltk
+import pandas as pd
+from spacy import displacy
+import spacy
 # -------------------------------------------------------------converting hindi audio file to text---------------------------------------------------------------------------
 
 def get_large_audio_transcription_hin(path='extracted.wav',r=sr.Recognizer()):
@@ -76,17 +78,41 @@ def text1():
     f2.write(whole_text)
     f2.close()
 
+def ner_extract():
+    global wait1
+    waitn = Label(root, text='Entity file generated', bg='white', fg='black', font=('Arial', 13,'bold'))
+    waitn.pack()
+    content = whole_text
+    print("loading model..")
+    model = spacy.load("en_core_web_trf")
+    print("model loaded")
+    doc = model(content)
+    entities = []
+    labels = []
+    start_pos = []
+    end_pos = []
+    for ent in doc.ents:
+        entities.append(ent)
+        labels.append(ent.label_)
+        start_pos.append(ent.start_char)
+        end_pos.append(ent.end_char)
+    df = pd.DataFrame({'Entities':entities,
+                   'Labels':labels,
+                   'Position_Start':start_pos,
+                   'Position_end':end_pos})
+    df.to_csv("Entities.csv")
+    
 
 root=Tk()
-root.title('Video to text Conversion')
+root.title('NER from Video&Audio')
 root.iconbitmap(r'video_fiu_icon.ico')
-root.geometry('1580x887')
+root.geometry('1500x700')
 
 
 # ---------extracting audio from video---------
 def video1():
     global wait1
-    wait1 = Label(root, text='Please wait a moment', bg='black', fg='white', font=('Arial', 13,'bold'))
+    wait1 = Label(root, text='Please wait a moment', bg='white', fg='black', font=('Arial', 13,'bold'))
     wait1.pack()
     video=askopenfilename()
     video=VideoFileClip(video)
@@ -97,7 +123,7 @@ def video1():
 # ---------extracting audio from video---------
 def video2():
     global wait2
-    wait2 = Label(text='Please wait a moment', bg='black', fg='white', font=('Arial', 13,'bold'))
+    wait2 = Label(text='Please wait a moment', bg='white', fg='black', font=('Arial', 13,'bold'))
     wait2.pack()
     video=askopenfilename()
     video=VideoFileClip(video)
@@ -109,28 +135,34 @@ def video2():
 # ----------audio Output (hindi)-------------
 def audio1():
     doc=gTTS(text=whole_text,lang='hi',slow=False)
-    doc.save('Output_audio.wav')
+    doc.save('Output_audio_hin.wav')
 
 
 # ----------audio Output (english)-------------
 def audio2():
     doc=gTTS(text=whole_text,lang='en',slow=False)
-    doc.save('Output_audio.wav')
+    doc.save('Output_audio_eng.wav')
 
-my_bg=ImageTk.PhotoImage(Image.open("bgd.png"))
+newsize = (1500, 700)
+im = Image.open(r"bgcol.png")
+im = im.resize((newsize), Image.ANTIALIAS)
+my_bg=ImageTk.PhotoImage(im)
 my_bglabel=Label(image=my_bg).place(x=-5,y=0)
-my_img=ImageTk.PhotoImage(Image.open("logo.png"))
+my_img=ImageTk.PhotoImage(Image.open("bglabel.png"))
 my_label=Label(image=my_img,border=0).pack(pady=100)
-my_text1=Label(root,text='Select video (HINDI)',bg="black",fg='white',font=('Arial',13,'bold')).place(x=1100,y=250)
-my_text2=Label(root,text='Select video (ENGLISH)',bg="black",fg='white',font=('Arial',13,'bold')).place(x=285,y=250)
-label = Label(root, text='Upload a Video', bg='black', fg='white', font=('Arial', 13)).pack()
-button1=Button(root,width=20,height=2,bg='#df514a',fg='white',border=0,text='Browse',font=('Arial',10,'bold'),command=video1).place(x=1100,y=300)
-button2=Button(root,width=20,height=2,bg='#df514a',fg='white',border=0,text='Browse',font=('Arial',10,'bold'),command=video2).place(x=300,y=300)
-button3=Button(root,width=30,height=2,bg='#df514a',fg='white',border=0,text='Download Audio file ↓\n(Hindi)',font=('Arial',15,'bold'),command=audio1).place(x=0,y=500)
-button4=Button(root,width=30,height=2,bg='#df514a',fg='white',border=0,text='Download Audio file ↓\n(English)',font=('Arial',15,'bold'),command=audio2).place(x=400,y=500)
-button5=Button(root,width=30,height=2,bg='#df514a',fg='white',border=0,text='Download Text file ↓\n(Hindi)',font=('Arial',15,'bold'),command=text1).place(x=800,y=500)
-button6=Button(root,width=30,height=2,bg='#df514a',fg='white',border=0,text='Download Text file ↓\n(English)',font=('Arial',15,'bold'),command=text2).place(x=1200,y=500)
-my_text3=Label(root,text='STEPS TO FOLLOW :-',bg="grey",fg='black',font=('Arial',15,'bold')).place(x=10,y=630)
-
+my_text1=Label(root,text='Select video (HINDI)',font=('Arial',13,'bold')).place(x=900,y=250)
+#label.config(bg='gray')
+#root.wm_attributes('', root['bg'])
+my_text2=Label(root,text='Select video (ENGLISH)',font=('Arial',13,'bold')).place(x=285,y=250)
+label = Label(root, text='Upload a Video', bg='white', fg='black', font=('Arial', 13,'bold')).pack()
+button1=Button(root,width=20,height=2,bg='#df514a',fg='white',border=2,text='Browse',font=('Arial',10,'bold'),command=video1).place(x=900,y=300)
+button2=Button(root,width=20,height=2,bg='#df514a',fg='white',border=2,text='Browse',font=('Arial',10,'bold'),command=video2).place(x=300,y=300)
+button3=Button(root,width=20,height=2,bg='#df514a',fg='white',border=2,text='Download Audio file ↓\n(Hindi)',font=('Arial',15,'bold'),command=audio1).place(x=20,y=500)
+button4=Button(root,width=20,height=2,bg='#df514a',fg='white',border=2,text='Download Audio file ↓\n(English)',font=('Arial',15,'bold'),command=audio2).place(x=350,y=500)
+button5=Button(root,width=20,height=2,bg='#df514a',fg='white',border=2,text='Download Text file ↓\n(Hindi)',font=('Arial',15,'bold'),command=text1).place(x=750,y=500)
+button6=Button(root,width=20,height=2,bg='#df514a',fg='white',border=2,text='Download Text file ↓\n(English)',font=('Arial',15,'bold'),command=text2).place(x=1100,y=500)
+#my_text3=Label(root,text='STEPS TO FOLLOW :-',bg="white",fg='black',font=('Arial',15,'bold')).place(x=10,y=630)
+#button7=Button(root,width=20,height=2,bg='#df514a',fg='white',border=2,text='Browse',font=('Arial',10,'bold'),command=).place(x=600,y=300)
+button7=Button(root,width=20,height=2,bg='#df514a',fg='white',border=2,text='Download MetaData',font=('Arial',15,'bold'),command=ner_extract).place(x=570,y=590)
 
 root.mainloop()
